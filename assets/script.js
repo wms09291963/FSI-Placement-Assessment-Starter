924fa022dfe7b9de2222b6516ce9e352000e7ef1
Smith <william.smith@bankerslife.com>
2022-02-16 19:54:17 -0600

    cookie_1

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,77 @@
 // HINT: You can delete this console.log after you no longer need it!
 console.log('JavaScript code has loaded!')
 // First, tell us your name
-let yourName = "Jane Doe" // HINT: Replace this with your own name!
+let yourName = "William Smith" // HINT: Replace this with your own name!
 
 // We'll use these variables to track the counts of each cookie type
 let gb = 0 // Gingerbread
 let cc = 0 // Chocolate Chip
 let sugar = 0 // Sugar Sprinkle
 
+//reference the text
+let gbQty = document.querySelector(".qty-gb")
+let ccQty = document.querySelector(" .qty-cc")
+let sugarQty=document.querySelector(".qty-sugar")
+
+
+
+
 // selecting the element with an id of credit
 const credit = document.querySelector('#credit')
-// selecting the element with an id of add-gb
+
+// selecting the element with an id of add-gb/minus and referencing the buttons
+const gbMinusBtn = document.querySelector('#minus-gb')
+const ccMinusBtn = document.querySelector('#minus-cc')
+const sugarMinusBtn = document.querySelector('#minus-sugar')
 const gbPlusBtn = document.querySelector('#add-gb')
+const ccPlusBtn = document.querySelector('#add-cc')
+const sugarPlusBtn = document.querySelector('#add-sugar')
+
 
 // Code to update name display
 credit.textContent = `Created by ${yourName}`
 
-// Event listener for clicks on the "+" button for Gingerbread cookies
+// Event listener for clicks on the "+" button for gb cookie
 gbPlusBtn.addEventListener('click', function() {
-// HINT: You can delete this console.log after you no longer need it!
-console.log('Gingerbread + button was clicked!')
+gb = gb + 1
 
-// TODO: Write the code to be run when the "+" button for "Gingerbread" is clicked
+gbQty.TextContent = "qty-gb: "+ gb
 })
 
-// TODO: Hook up event listeners for the rest of the buttons
+// Event listener for clicks on the "+" button for cc cookie
+ccPlusBtn.addEventListener('click', function() {
+    cc = cc + 1
+    
+    ccQty.TextContent = "qty-cc: "+ cc
+    })
+// Event listener for clicks on the "+" button for sugar cookie
+gbPlusBtn.addEventListener('click', function() {
+    sugar = sugar + 1
+    
+    sugarQty.TextContent = "qty-sugar: "+ sugar
+  })
+
+    
+gbminusButton.addEventListener("click", function(){
+
+    if (gb > 0 ){
+   gb = gb-1  
+   quantityText.textContent = "Quantity: "+ totalQuantity
+    }
+})
+
+ccminusButton.addEventListener("click", function(){
+
+    if (cc > 0 ){
+   cc = cc-1  
+   quantityText.textContent = "Quantity: " - totalQuantity
+    }
+})
+
+sugarminusButton.addEventListener("click", function(){
+
+    if (sugar > 0 ){
+   sugar = sugar-1  
+   quantityText.textContent = "Quantity: " - totalQuantity
+    }
+})
